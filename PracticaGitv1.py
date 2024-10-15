d50c34e2ceb2b618f8a58c901edde9c87dcae70a
Alfredo Feria Guzman <116789213+TuGorditoSensualon16@users.noreply.github.com>
2024-10-15 12:39:30 -0600

    Practica GitHub en Formado python v1
    Clonamos el positorio

---
+++ PracticaGitv1.py
@@ -0,0 +1,16 @@
+#Practica para clonar un repositorio de GitHub
+#Creado por: Creado por Alfredo Feria y Juan Andres Hernandes 
+#VERSION 1.0 Hello Word en Python
+
+
+
+#Creamos una fucnion con un print que imprime un mensaje
+def hello ():
+    print("Hello Word")
+
+
+
+
+#Llamamos a la funcion hello
+if __name__ == "__main__":
+    hello()
